2137d1b9b5cfe81d8b27b56ebb6b9cfcf4acbcdc
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-05-16 21:56:43 -0300

    Começando com entity framework -> Entity Framework: Banco de dados de forma eficiente

---
+++ Programacao/Cursos_de_C#/Entity_Framework/LojaComEntity/LojaComEntity/EntidadeContext.cs
@@ -0,0 +1,24 @@
+﻿using LojaComEntity.Entidades;
+using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace LojaComEntity
+{
+    public class EntidadeContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var stringConexao = ConfigurationManager.ConnectionStrings["lojaConnectionString"].ConnectionString;
+
+            optionsBuilder.UseSqlServer(stringConexao);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ Programacao/Cursos_de_C#/Entity_Framework/LojaComEntity/LojaComEntity/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace LojaComEntity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Rodou");
+            //Console.ReadLine();
+            //IDbConnection con = new SqlConnection("");
+            //IDbCommand command = con.CreateCommand();
+            //command.CommandText = "select * from usuario";
+            //command.CommandType = CommandType.Text;
+            //IDataReader leitor = command.ExecuteReader();
+
+            //while (leitor.Read())
+            //{
+            //    int id = Convert.ToInt32( leitor["id"]);
+            //    string nome = Convert.ToString(leitor["id"]);
+            //}
+
+            EntidadeContext contexto = new EntidadeContext();
+
+            var usuario = new Entidades.Usuario()
+            {
+                Nome = "Marcio",
+                Senha = "123"
+            };
+
+            contexto.Usuarios.Add(usuario);
+            contexto.SaveChanges();
+            contexto.Dispose();
+
+        }
+    }
+}
