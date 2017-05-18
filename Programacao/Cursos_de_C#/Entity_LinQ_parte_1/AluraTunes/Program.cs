d0bcba4728c9223c734c6a8f70436bb3e77958f7
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-05-18 14:00:05 -0300

    1 - Linq to Xml - 2 - Linq to XML  - Entity LinQ parte 1: Crie queries poderosas em C#

--- Programacao/Cursos_de_C#/Entity_LinQ_parte_1/AluraTunes/Program.cs
+++ Programacao/Cursos_de_C#/Entity_LinQ_parte_1/AluraTunes/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace AluraTunes
 {
@@ -10,6 +11,7 @@ namespace AluraTunes
         static void Main(string[] args)
         {
             //listar os gêneros rock
+            #region
             var generos = new List<Genero>() {
                 new Genero { ID = 1 , Nome = "Rock" },
                 new Genero { ID = 2, Nome = "Raggae" },
@@ -18,7 +20,7 @@ namespace AluraTunes
                 new Genero { ID = 5, Nome = "Clássica" }
             };
             //forma 1
-            foreach(var genero in generos)
+            foreach (var genero in generos)
             {
                 if (genero.Nome.Contains("Rock"))
                     Console.WriteLine("ID {0}\tgênero {1}", genero.ID, genero.Nome);
@@ -30,7 +32,9 @@ namespace AluraTunes
                 Console.WriteLine("ID {0}\tgênero {1}", genero.ID, genero.Nome);
             }
             Console.WriteLine("--------------------------------------------------");
+            #endregion
             //listar músicas do sistema
+            #region
             var musicas = new List<Musica>
             {
                 new Musica{ ID = 1 , Nome = "Sweet ChildO'Mine" , GeneroID= 1},
@@ -44,35 +48,34 @@ namespace AluraTunes
             }
             Console.WriteLine("--------------------------------------------------");
             //Forma 2
-            var musicaGenero = from m 
+            var musicaGenero = from m
                                  in musicas
-                                 join g in generos on m.GeneroID equals g.ID
-                             select new { m , g};
+                               join g in generos on m.GeneroID equals g.ID
+                               select new { m, g };
             foreach (var musica in (from m in musicaGenero select m))
             {
                 Console.WriteLine("ID {0}\tmúsica {1}\tgênero{2}", musica.m.ID, musica.m.Nome, musica.g.Nome);
             }
+            Console.WriteLine("--------------------------------------------------");
+            #endregion
 
+            #region
+            List<Genero> generos1 = new List<Genero>()
+            {
+                new Genero { ID = 1, Nome = "Rock" },
+                new Genero { ID = 2, Nome = "Reggae" },
+                new Genero { ID = 3, Nome = "Rock Progressivo" },
+                new Genero { ID = 4, Nome = "Jazz" },
+                new Genero { ID = 5, Nome = "Punk Rock" },
+                new Genero { ID = 6, Nome = "Classica" }
+            };
 
-
-
-
-            List<Genero> generos1 = new List<Genero>
-        {
-            new Genero { ID = 1, Nome = "Rock" },
-            new Genero { ID = 2, Nome = "Reggae" },
-            new Genero { ID = 3, Nome = "Rock Progressivo" },
-            new Genero { ID = 4, Nome = "Jazz" },
-            new Genero { ID = 5, Nome = "Punk Rock" },
-            new Genero { ID = 6, Nome = "Classica" }
-        };
-
-            List<Musica> musicas1 = new List<Musica>
-        {
-            new Musica { ID = 1, Nome = "Sweet Child O'Mine", GeneroID = 1 },
-            new Musica { ID = 2, Nome = "I Shot The Sheriff", GeneroID = 2 },
-            new Musica { ID = 3, Nome = "Danúbio Azul", GeneroID = 6 }
-        };
+            List<Musica> musicas1 = new List<Musica>()
+            {
+                new Musica { ID = 1, Nome = "Sweet Child O'Mine", GeneroID = 1 },
+                new Musica { ID = 2, Nome = "I Shot The Sheriff", GeneroID = 2 },
+                new Musica { ID = 3, Nome = "Danúbio Azul", GeneroID = 6 }
+            };
 
             //Crie uma consulta para listar os nomes das músicas cujo gênero tenha o nome "Reggae".
 
@@ -82,6 +85,36 @@ namespace AluraTunes
             {
                 Console.WriteLine(item);
             }
+
+            Console.WriteLine("--------------------------------------------------");
+            #endregion
+
+            //Variável que vai representar a raiz do XML
+            //no=> AluraTunes
+            XElement root = XElement.Load(string.Format("{0}{1}", System.AppDomain.CurrentDomain.BaseDirectory.ToString().Replace(@"bin\Debug\",""), @"\Data\AluraTunes.xml"));
+            //Definir consulta linq para acessar o dado xml
+            //no=> Generos , subno => Genero
+            var qyeryXMLGenero = from g in root.Element("Generos").Elements("Genero") select g;
+
+            foreach(XElement genefo in qyeryXMLGenero)
+            {
+                Console.WriteLine("Genero: " + genefo.Element("GeneroId").Value + " Nome: " + genefo.Element("Nome").Value);
+            }
+            Console.WriteLine("--------------------------------------------------");
+            var qyeryXMLMusicaEGenero = from g in root.Element("Generos").Elements("Genero")
+                                        join m in root.Element("Musicas").Elements("Musica")
+                                               on g.Element("GeneroId").Value equals m.Element("GeneroId").Value
+                                        select new
+                                        {
+                                            Musica = m.Element("Nome").Value,
+                                            Genero = g.Element("Nome").Value
+                                        };
+
+            foreach (var item in qyeryXMLMusicaEGenero)
+            {
+                Console.WriteLine("{0}\t{1}", item.Musica, item.Genero);
+            }
+            Console.WriteLine("--------------------------------------------------");
 
             Console.ReadLine();
         }
