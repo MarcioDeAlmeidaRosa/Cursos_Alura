9a78fda8445030382deedd3a07b41408395f0ab0
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-05-17 23:14:48 -0300

    02 - 1- Introdução ao Linq com select from e where - Linq to Objects  -  Entity LinQ parte 1: Crie queries poderosas em C#

---
+++ Programacao/Cursos_de_C#/Entity_LinQ_parte_1/AluraTunes/Program.cs
@@ -0,0 +1,38 @@
+﻿using AluraTunes.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AluraTunes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //listar os gêneros rock
+            var generos = new List<Genero>() {
+                new Genero { ID = 1 , Nome = "Rock" },
+                new Genero { ID = 2, Nome = "Raggae" },
+                new Genero { ID = 3, Nome = "Rock Progressivo" },
+                new Genero { ID = 4, Nome = "Punk Rock" },
+                new Genero { ID = 5, Nome = "Clássica" }
+            };
+            //forma 1
+            foreach(var genero in generos)
+            {
+                if (genero.Nome.Contains("Rock"))
+                    Console.WriteLine("ID {0}\tgênero {1}", genero.ID, genero.Nome);
+            }
+            Console.WriteLine("--------------------------------------------------");
+            //forma 2
+            foreach (var genero in (from g in generos where g.Nome.Contains("Rock") select g))
+            {
+                if (genero.Nome.Contains("Rock"))
+                    Console.WriteLine("ID {0}\tgênero {1}", genero.ID, genero.Nome);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
