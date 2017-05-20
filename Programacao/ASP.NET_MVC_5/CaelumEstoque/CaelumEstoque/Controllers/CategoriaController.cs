41d090fdbb7149d38565406daa9807c9eac3f60c
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-05-20 10:01:10 -0300

    06 - Lista de categorias - 03 - Controlando Requisições e a Listagem de Produtos  - ASP.NET MVC 5: O framework pronto para a web

---
+++ Programacao/ASP.NET_MVC_5/CaelumEstoque/CaelumEstoque/Controllers/CategoriaController.cs
@@ -0,0 +1,20 @@
+﻿using CaelumEstoque.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        // GET: Categoria
+        public ActionResult Index()
+        {
+            CategoriasDAO dao = new CategoriasDAO();
+            ViewBag.Categorias = dao.Lista();
+            return View();
+        }
+    }
+}
