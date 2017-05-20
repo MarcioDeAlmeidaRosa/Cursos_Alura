cf71fbdd4451fef9e2980cdada3e4081561af897
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-05-20 10:30:48 -0300

    01 - Vídeo 1 - 04 - Lidando com Formulários e o Cadastro de Produtos - ASP.NET MVC 5: O framework pronto para a web

--- Programacao/ASP.NET_MVC_5/CaelumEstoque/CaelumEstoque/Controllers/ProdutoController.cs
+++ Programacao/ASP.NET_MVC_5/CaelumEstoque/CaelumEstoque/Controllers/ProdutoController.cs
@@ -1,4 +1,5 @@
 ﻿using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,5 +17,28 @@ namespace CaelumEstoque.Controllers
             ViewBag.Produtos = dao.Lista();
             return View();
         }
+
+        public ActionResult Form()
+        {
+            ViewBag.Categorias = new CategoriasDAO().Lista();
+            return View();
+        }
+
+        [HttpPost]//-->Permte somente requisição Post
+        //public ActionResult Adiciona(string nome, float preco, int quantidade, string descricao, int categoriaId)
+        public ActionResult Adiciona(Produto produto)//<- Model Binder faz a criação do produto no cliente antes de enviar para o servidor
+        {
+            //Produto produto = new Produto();
+            //produto.Nome = nome;
+            //produto.Preco = preco;
+            //produto.Quantidade = quantidade;
+            //produto.Descricao = descricao;
+            //produto.CategoriaId = categoriaId;
+            ProdutosDAO dao = new ProdutosDAO();
+            dao.Adiciona(produto);
+            //return View();
+            return RedirectToAction("Index", "Produto");
+        }
+            
     }
 }
