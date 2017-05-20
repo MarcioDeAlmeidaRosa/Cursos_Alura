2fe26caef76affc6002da4413e5fb587e1a64429
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-05-20 09:47:53 -0300

    01 - Vídeo 1 - 03 - Controlando Requisições e a Listagem de Produtos  - ASP.NET MVC 5: O framework pronto para a web

---
+++ Programacao/ASP.NET_MVC_5/CaelumEstoque/CaelumEstoque/Controllers/ProdutoController.cs
@@ -0,0 +1,20 @@
+﻿using CaelumEstoque.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        public ActionResult Index()
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            ViewBag.Produtos = dao.Lista();
+            return View();
+        }
+    }
+}
