4398f1ace27bdfaacfb1eccffdc10136a54e467a
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-05-20 22:44:50 -0300

    01 - Aplicando layout e estilo bootstrap - Inclusão das funcionalidades de usuário - 08 - Layouts com o Twitter Bootstrap - ASP.NET MVC 5: O framework pronto para a web

---
+++ Programacao/ASP.NET_MVC_5/CaelumEstoque/CaelumEstoque/Controllers/UsuarioController.cs
@@ -0,0 +1,38 @@
+﻿using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new UsuariosDAO().Lista());
+        }
+
+        [HttpGet]
+        public ActionResult Form()
+        {
+            return View(new Usuario());
+        }
+
+        [HttpPost]
+        public ActionResult Adicionar(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                new UsuariosDAO().Adiciona(usuario);
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Form", usuario);
+        }
+    }
+}
