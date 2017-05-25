6336f32db1733b1669a170443995ba9def011102
Marcio De Almeida Rosa <devel02@GRMLT.LOCAL>
2017-05-25 18:54:49 -0300

    10 - Utilizando o EJS - 01 - Instalação do NodeJs - Node.js: Inovando com Javascript no backend

--- Formacao_front_end/Frameworks_MVC/Curso_Node_js/CasaDoCodigo/app.js
+++ Formacao_front_end/Frameworks_MVC/Curso_Node_js/CasaDoCodigo/app.js
@@ -5,9 +5,17 @@
 var express = require('express');
 //Criação do objeto express em memória para ser utilizado
 var app = express();
+
+//set -> usado para definir variáveis que pode ser usada 
+//por todo o escopo do sistema
+//configurando a engeni como ejs
+app.set('view engine', 'ejs');
+
 //configurando a rota --> o metodo e conteúdo que iremos atender
 app.get('/produtos', function(req,res){
-    res.send('<html><head><meta charset="UTF-8"></head><body><h1>Listando os produtos da loja...</h1></body><html>');
+    //usando ejs
+    res.render("produtos/lista");
+    //res.send('<html><head><meta charset="UTF-8"></head><body><h1>Listando os produtos da loja...</h1></body><html>');
 });
 //configurando a porta que o servidor que acabamos de criar irá escutar
 app.listen(3000, function(){
