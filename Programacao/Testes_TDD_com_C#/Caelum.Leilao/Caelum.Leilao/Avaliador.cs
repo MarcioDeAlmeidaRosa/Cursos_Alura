f371c71c641a7378cf18c9e9a8ea4cffff3a7c2d
Marcio de Almeida Rosa <marcio.rosa@grupomalta.com.br>
2017-06-06 13:13:43 -0300

    01 - Aula - 07 - Animações simples e eventos - jQuery: Manipulação dinâmica de conteúdo

---
+++ Programacao/Testes_TDD_com_C#/Caelum.Leilao/Caelum.Leilao/Avaliador.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caelum.Leilao
+{
+    public class Avaliador
+    {
+        private double maiorDeTodos = double.MinValue;
+        private double menorValor = double.MaxValue;
+
+        public void Avalia(Leilao leilao)
+        {
+            foreach(var lance in leilao.Lances)
+            {
+                if (lance.Valor > maiorDeTodos)
+                    maiorDeTodos = lance.Valor;
+                if (lance.Valor < menorValor)
+                    menorValor = lance.Valor;
+            }
+        }
+
+        public double MaiorDeTodos
+        {
+            get { return maiorDeTodos; }
+        }
+
+        public double MenorDeTodos
+        {
+            get { return menorValor; }
+        }
+    }
+}
