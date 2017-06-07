39f38584f8b0258a2ef1f148f15402d2a78ee8c4
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-06-07 00:20:40 -0300

    01 - Reiniciando o jogo - 04 - Reiniciando o jogo - jQuery: Domine a biblioteca mais popular do mercado

--- Formacao_front_end/jQuery/alura-typer/public/js/main.js
+++ Formacao_front_end/jQuery/alura-typer/public/js/main.js
@@ -1,23 +1,34 @@
-console.log("olá mundo");
-var frase = jQuery(".frase").text();
-console.log(frase);
+var tempoInicial = parseInt($(".tempo-digitacao").text());
+var campo = $(".campo-digitacao");
 
-var totalPalavras = frase.split(" ");
-var tamanho = totalPalavras.length;
-console.log(totalPalavras.length);
-// recuperando todos os li filhos de informacoes
-// recuperando o primeiro e alterando o texto
-// usando ternário para colocar plural na descrição do contador
-$(".informacoes").children("li").first().text(tamanho + " palavra" + ((tamanho > 1) ? "s" : ""));
+//função que só será executada quando 
+//todo o documento estiver carregado
+// $(document).ready();
+//podemos usar de forma resumida
+$(function() {
+    atualizaTamanhoFrase();
+    inicializaCronometro();
+    inicializaContadores();
+    $("#reiniciar").click(reiniciaJogo);
+});
 
-//$(".campo-digitacao").on("click", function() {
-$(".campo-digitacao").on("input", function() {
-    var palavras = $(this).val().trim().split(/\S+/);
+function atualizaTamanhoFrase() {
+    var frase = jQuery(".frase").text();
+    var totalPalavras = frase.split(" ");
+    var tamanho = totalPalavras.length;
+    // recuperando todos os li filhos de informacoes
+    // recuperando o primeiro e alterando o texto
+    // usando ternário para colocar plural na descrição do contador
+    $(".informacoes").children("li").first().text(tamanho + " palavra" + ((tamanho > 1) ? "s" : ""));
+}
 
-    $(".contador-caracteres").text($(this).val().length);
+function inicializaTotais() {
+    var palavras = campo.val().trim().split(/\S+/);
+
+    $(".contador-caracteres").text(campo.val().length);
     $(".contador-palavras").text(palavras.length - 1);
 
-    if ($(this).val().trim().length > 1)
+    if (campo.val().trim().length > 1)
         $(".s-contador-caracteres").show();
     else
         $(".s-contador-caracteres").hide();
@@ -26,22 +37,36 @@ $(".campo-digitacao").on("input", function() {
         $(".s-contador-palavras").show();
     else
         $(".s-contador-palavras").hide();
-});
+}
 
-var tempoRestante = parseInt($(".tempo-digitacao").text());
-console.log(tempoRestante);
-// $(".campo-digitacao").on("focus", function() {
-//função one similar a on -> porém só vai escutar o evento 1 única vez
-//já a on, toda vez que ele se repetir, será executado
-$(".campo-digitacao").one("focus", function() {
-    var id = setInterval(function() {
-        tempoRestante--;
-        $(".tempo-digitacao").text(tempoRestante);
-        if (tempoRestante === 0) {
-            $(".campo-digitacao").attr("disabled", true);
-            //para a execução do setInterval
-            clearInterval(id);
-        }
-        //1000 milesegundos = 1 segundo
-    }, 1000);
-});
+function inicializaContadores() {
+    //campo.on("click", function() {
+    campo.on("input", inicializaTotais);
+}
+
+function inicializaCronometro() {
+    var tempoRestante = tempoInicial;
+    // campo.on("focus", function() {
+    //função one similar a on -> porém só vai escutar o evento 1 única vez
+    //já a on, toda vez que ele se repetir, será executado
+    campo.one("focus", function() {
+        var id = setInterval(function() {
+            tempoRestante--;
+            $(".tempo-digitacao").text(tempoRestante);
+            if (tempoRestante === 0) {
+                campo.attr("disabled", true);
+                //para a execução do setInterval
+                clearInterval(id);
+            }
+            //1000 milesegundos = 1 segundo
+        }, 1000);
+    });
+}
+
+function reiniciaJogo() {
+    campo.attr("disabled", false);
+    campo.val("");
+    inicializaTotais();
+    $(".tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+}
