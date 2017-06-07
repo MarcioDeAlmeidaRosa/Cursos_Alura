f79a8d8b5ebae0a76ac8c577a8996b96e62640fc
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-06-06 23:06:43 -0300

    01 - Primeiros passos com jQuery - 02 - Escutando eventos com jQuery - jQuery: Domine a biblioteca mais popular do mercado

--- Formacao_front_end/jQuery/alura-typer/public/js/main.js
+++ Formacao_front_end/jQuery/alura-typer/public/js/main.js
@@ -5,4 +5,26 @@ console.log(frase);
 var totalPalavras = frase.split(" ");
 var tamanho = totalPalavras.length;
 console.log(totalPalavras.length);
+// recuperando todos os li filhos de informacoes
+// recuperando o primeiro e alterando o texto
+// usando ternário para colocar plural na descrição do contador
 $(".informacoes").children("li").first().text(tamanho + " palavra" + ((tamanho > 1) ? "s" : ""));
+
+//$(".campo-digitacao").on("click", function() {
+$(".campo-digitacao").on("input", function() {
+    var palavras = $(this).val().trim().split(/\S+/);
+
+    $(".contador-caracteres").text($(this).val().length);
+    $(".contador-palavras").text(palavras.length - 1);
+
+    if ($(this).val().trim().length > 1)
+        $(".s-contador-caracteres").show();
+    else
+        $(".s-contador-caracteres").hide();
+
+    if ((palavras.length - 1) > 1)
+        $(".s-contador-palavras").show();
+    else
+        $(".s-contador-palavras").hide();
+
+});
