b25a0db280408dc0ac686f0e5e2539bd3ed7fec1
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-06-06 22:13:44 -0300

    04 - Primeiros passos com jQuery - 01 - Introdução - jQuery: Domine a biblioteca mais popular do mercado

--- Formacao_front_end/jQuery/alura-typer/public/js/main.js
+++ Formacao_front_end/jQuery/alura-typer/public/js/main.js
@@ -1,1 +1,8 @@
 console.log("olá mundo");
+var frase = jQuery(".frase").text();
+console.log(frase);
+
+var totalPalavras = frase.split(" ");
+var tamanho = totalPalavras.length;
+console.log(totalPalavras.length);
+$(".informacoes").children("li").first().text(tamanho + " palavra" + ((tamanho > 1) ? "s" : ""));
