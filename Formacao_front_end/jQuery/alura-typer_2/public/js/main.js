7ca6c6fd680b4dccf435a8fb7ae47b50bf0d91ce
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-06-08 23:20:03 -0300

    06 - Existem 1000 maneiras de se fazer... - 01 - Animações com jQuery  - jQuery: Avance na biblioteca mais popular do mercado parte 2

--- Formacao_front_end/jQuery/alura-typer_2/public/js/main.js
+++ Formacao_front_end/jQuery/alura-typer_2/public/js/main.js
@@ -81,3 +81,46 @@ function reiniciaJogo() {
 function ScrolFocoPrincipal(elementoFoco, elementoReferencia) {
     $(elementoFoco).animate({ scrollTop: $(elementoReferencia).offset().top + "px" }, 1000);
 };
+
+
+
+/*
+$('#botao-promocao').click(function() {
+  var promocoes = $('.promocoes');
+  if(promocoes.is(':visible')) {
+    promocoes.hide();
+  } else {
+    promocoes.show();
+  }
+});
+
+O jQuery possui a função is que permite consultar uma pseudo class. Toda vez que um elemento esta com display diferente de none ele ganha a pseudo classe :visible . A função is retorna true caso o elemento esteja visível. Se ele estiver visível, precisamos escondê-lo e isso é feito através da função hide. Para exibir o elemento, é usada a função show.
+Alternativa correta
+
+$('#botao-promocao').click(function() {
+  $('.promocoes').toggleClass('invisivel');
+});
+
+A função toogleClass (não confundir com a função toggle) adiciona uma classe caso ela não exista no elemento. Se existir, ela remove a classe.
+Alternativa correta
+
+$('#botao-promocao').click(function() {
+  $('.promocoes').toggle();
+});
+
+A função toggle é um atalho para as funções hide e show. Quando ela é chamada para um elemento visível, o elemento fica invisível. Quando é chamada para um elemento invisível, ela torna o elemento visível.
+Alternativa correta
+
+$('#botao-promocao').click(function() {
+  var promocoes = $('.promocoes');
+  if(promocoes.hasClass('invisivel')) {
+    promocoes.removeClass('invisivel');
+  } else {
+    promocoes.addClass('invisivel');
+  }
+});
+
+Esta correta! o jQuery possui a função hasClass que retorna true se um elemento possui ou não uma classe. Na condição, removemos a classe invisivel caso o elemento já a tenha e a adicionamos caso ele não a tenha. Todo esse processo é feito a cada clique do usuário.
+
+
+*/
