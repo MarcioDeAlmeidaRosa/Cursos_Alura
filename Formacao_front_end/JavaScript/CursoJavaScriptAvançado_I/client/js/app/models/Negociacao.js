2161f03884d19596bbf226288e0fba074cade801
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-06-13 20:50:00 -0300

    03 - Construtor da classe e métodos - 02 - Especificando uma Negociação - JavaScript avançado I: ES6, orientação a objetos e padrões de projetos

--- Formacao_front_end/JavaScript/CursoJavaScriptAvançado_I/client/js/app/models/Negociacao.js
+++ Formacao_front_end/JavaScript/CursoJavaScriptAvançado_I/client/js/app/models/Negociacao.js
@@ -2,16 +2,21 @@
 class Negociacao {
     //Quando definimos uma classe com contrutor,
     //só podemos chama-la usando o perador new
-    constructor() {
+    constructor(data, quantidade, valor) {
         //quando usamos o operador new,
         //vai tornar o this correspondente
         //ao objeto criado
-        this.data = new Date();
-        this.quantidade = 1;
-        this.valor = 0;
+        this.data = data;
+        this.quantidade = quantidade;
+        this.valor = valor;
+        // this.volume = (this.quantidade * this.valor);
         //this --> é uma variável implicita
         //que sempre aponta para a instância
         //que esta executando a operação
         //naquele momento
+    }
+
+    obtemValume() {
+        return this.quantidade * this.valor;
     }
 }
