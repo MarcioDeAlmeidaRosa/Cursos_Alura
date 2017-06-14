7c9768b7e5f55ce99860af596b4a6e562fbebf0a
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-06-14 00:11:24 -0300

    09 - Arrow Functions: deixando o código ainda menos verboso - 03 - A ligação entre as ações do usuário e o modelo  - JavaScript avançado I: ES6, orientação a objetos e padrões de projetos

--- Formacao_front_end/JavaScript/CursoJavaScriptAvançado_I/client/js/app/controllers/NegociacaoController.js
+++ Formacao_front_end/JavaScript/CursoJavaScriptAvançado_I/client/js/app/controllers/NegociacaoController.js
@@ -23,53 +23,67 @@ class NegociacaoController {
     }
 
     adiciona(event) {
-        event.preventDefault();
-        // alert("chamei ação n controller");
+            event.preventDefault();
+            // alert("chamei ação n controller");
 
-        console.log(typeof(this._inputDate.value));
+            console.log(typeof(this._inputDate.value));
 
-        console.log(this._inputDate.value);
-        console.log("//1º - Forma - usando aplit");
-        console.log(new Date(this._inputDate.value.split("-")));
-        console.log("//2º - Forma - usando replace com regex");
-        console.log(new Date(this._inputDate.value.replace(/-/g, ',')));
-        console.log("//3º - Forma - usando split recuperando as casas");
-        let ano = parseInt(this._inputDate.value.split("-")[0]);
-        let mes = parseInt(this._inputDate.value.split("-")[1]) - 1;
-        let dia = parseInt(this._inputDate.value.split("-")[2]);
-        console.log(new Date(ano, mes, dia));
-        console.log("//4º - Forma - usando spread operator");
-        console.log(
-            new Date(
-                this._inputDate.value
-                .split("-")
-                .map(function(item, indice) {
-                    if (indice == 1)
-                        return item - 1;
-                    return item;
-                })
-            ));
-        console.log("//5º - Forma - usando spread operator - e calculo de módulo");
-        console.log(
-            new Date(
-                this._inputDate.value
-                .split("-")
-                .map(function(item, indice) {
-                    return item - (item % 2);
-                })
-            ));
+            console.log(this._inputDate.value);
+            console.log("//1º - Forma - usando aplit");
+            console.log(new Date(this._inputDate.value.split("-")));
+            console.log("//2º - Forma - usando replace com regex");
+            console.log(new Date(this._inputDate.value.replace(/-/g, ',')));
+            console.log("//3º - Forma - usando split recuperando as casas");
+            let ano = parseInt(this._inputDate.value.split("-")[0]);
+            let mes = parseInt(this._inputDate.value.split("-")[1]) - 1;
+            let dia = parseInt(this._inputDate.value.split("-")[2]);
+            console.log(new Date(ano, mes, dia));
+            console.log("//4º - Forma - usando spread operator");
+            console.log(
+                new Date(
+                    this._inputDate.value
+                    .split("-")
+                    .map(function(item, indice) {
+                        if (indice == 1)
+                            return item - 1;
+                        return item;
+                    })
+                ));
+            console.log("//5º - Forma - usando spread operator - e calculo de módulo");
+            console.log(
+                new Date(
+                    this._inputDate.value
+                    .split("-")
+                    .map(function(item, indice) {
+                        return item - (item % 2);
+                    })
+                ));
+            console.log("//6º - Forma - usando arrow arrow functions - e calculo de módulo");
+            console.log(
+                new Date(
+                    this._inputDate.value
+                    .split("-")
+                    .map((item, indice) => {
+                        return item - (item % 2);
+                    })
+                ));
+            console.log("//7º - Forma - usando arrow arrow functions simplificada por só ter uma instrução - e calculo de módulo");
+            console.log(
+                new Date(
+                    this._inputDate.value
+                    .split("-")
+                    .map((item, indice) => item - (item % 2));
 
-        console.log(this._inputQuantidade.value);
-        console.log(this._inputValor.value);
+                    console.log(this._inputQuantidade.value); console.log(this._inputValor.value);
 
-        let negocicacao = new Negociacao(
-            new Date(this._inputDate.value.split("-")),
-            this._inputQuantidade.value,
-            this._inputValor.value
-        );
+                    let negocicacao = new Negociacao(
+                        new Date(this._inputDate.value.split("-")),
+                        this._inputQuantidade.value,
+                        this._inputValor.value
+                    );
 
-        //adicionar uma negociação em uma lista
+                    //adicionar uma negociação em uma lista
 
-        console.log(negocicacao);
-    }
-}
+                    console.log(negocicacao);
+                }
+            }
