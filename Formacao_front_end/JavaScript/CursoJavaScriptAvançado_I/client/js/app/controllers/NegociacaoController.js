6c36e660efebba8fc291e398859dcb3141c76f80
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-06-13 22:44:29 -0300

    02 - Associando métodos do controller às ações do usuário - 03 - A ligação entre as ações do usuário e o modelo  - JavaScript avançado I: ES6, orientação a objetos e padrões de projetos

--- Formacao_front_end/JavaScript/CursoJavaScriptAvançado_I/client/js/app/controllers/NegociacaoController.js
+++ Formacao_front_end/JavaScript/CursoJavaScriptAvançado_I/client/js/app/controllers/NegociacaoController.js
@@ -7,5 +7,27 @@ class NegociacaoController {
         event.preventDefault();
         // alert("chamei ação n controller");
 
+        //Quando atribuímos a função ou "método da classe document"
+        //a referência que é atribuída perde a referência this
+        //pois somente a função é "copiada",
+        //para corrigir isso, temos o auxílio do metodo
+        //bind, que define a referência chamadora, seu "this",
+        //no caso document, assim quando jogamos a referência
+        //para a variável $, ao chamar a função
+        //seu this continuará sendo document, 
+        //se comportando normalmente,
+        //algo similar é feito com jQuery
+        let $ = document.querySelector.bind(document);
+
+        let inputDate = $("#data");
+        let inputQuantidade = $("#quantidade");
+        let inputValor = $("#valor");
+
+        console.log(inputDate.value);
+        console.log(inputQuantidade.value);
+        console.log(inputValor.value);
+
+
+
     }
 }
