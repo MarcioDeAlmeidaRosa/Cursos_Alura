0bbaa55a60a42b1ef42ca09faff7bf5ffdff6b02
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-06-13 22:30:17 -0300

    01 - A classe NegociacaoController - 03 - A ligação entre as ações do usuário e o modelo  - JavaScript avançado I: ES6, orientação a objetos e padrões de projetos

---
+++ Formacao_front_end/JavaScript/CursoJavaScriptAvançado_I/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,11 @@
+class NegociacaoController {
+    //     constructor() {
+
+    //     }
+
+    adiciona(event) {
+        event.preventDefault();
+        // alert("chamei ação n controller");
+
+    }
+}
