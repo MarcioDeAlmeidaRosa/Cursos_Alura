ff848cce62fd7dad979f6a2ec2ecd83e243bdb91
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-06-19 20:38:24 -0300

    02 - Isolando a responsabilidade de conversão de datas - 04 Lidar com data é trabalhoso? Chame um ajudante! - avaScript avançado I: ES6, orientação a objetos e padrões de projetos

--- Formacao_front_end/JavaScript/CursoJavaScriptAvançado_I/client/js/app/controllers/NegociacaoController.js
+++ Formacao_front_end/JavaScript/CursoJavaScriptAvançado_I/client/js/app/controllers/NegociacaoController.js
@@ -24,70 +24,11 @@ class NegociacaoController {
 
     adiciona(event) {
         event.preventDefault();
-        // alert("chamei ação n controller");
 
-        console.log(typeof(this._inputDate.value));
-
-        console.log(this._inputDate.value);
-        console.log("//1º - Forma - usando aplit");
-        console.log(new Date(this._inputDate.value.split("-")));
-        console.log("-------------------------------------------------------------------------------------------------");
-        console.log("//2º - Forma - usando replace com regex");
-        console.log(new Date(this._inputDate.value.replace(/-/g, ',')));
-        console.log("-------------------------------------------------------------------------------------------------");
-        console.log("//3º - Forma - usando split recuperando as casas");
-        let ano = parseInt(this._inputDate.value.split("-")[0]);
-        let mes = parseInt(this._inputDate.value.split("-")[1]) - 1;
-        let dia = parseInt(this._inputDate.value.split("-")[2]);
-        console.log(new Date(ano, mes, dia));
-        console.log("-------------------------------------------------------------------------------------------------");
-        console.log("//4º - Forma - usando spread operator");
-        console.log(
-            new Date(
-                this._inputDate.value
-                .split("-")
-                .map(function(item, indice) {
-                    if (indice == 1)
-                        return item - 1;
-                    return item;
-                })
-            ));
-        console.log("-------------------------------------------------------------------------------------------------");
-        console.log("//5º - Forma - usando spread operator - e calculo de módulo");
-        console.log(
-            new Date(
-                this._inputDate.value
-                .split("-")
-                .map(function(item, indice) {
-                    return item - (item % 2);
-                })
-            ));
-        console.log("-------------------------------------------------------------------------------------------------");
-        console.log("//6º - Forma - usando arrow functions - e calculo de módulo");
-        console.log(
-            new Date(
-                this._inputDate.value
-                .split("-")
-                .map((item, indice) => {
-                    return item - (item % 2);
-                })
-            ));
-        console.log("-------------------------------------------------------------------------------------------------");
-
-        console.log("//7º - Forma - usando arrow functions simplificada por só ter uma instrução - e calculo de módulo");
-        console.log(
-            new Date(
-                this._inputDate.value
-                .split("-")
-                .map((item, indice) => item - (item % 2))
-            ));
-
-        console.log(this._inputQuantidade.value);
-        console.log(this._inputValor.value);
-        console.log("-------------------------------------------------------------------------------------------------");
+        let helper = new DateHelper();
 
         let negocicacao = new Negociacao(
-            new Date(this._inputDate.value.split("-")),
+            helper.textToDate(this._inputDate.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
@@ -96,8 +37,6 @@ class NegociacaoController {
         //adicionar uma negociação em uma lista
 
         console.log(negocicacao);
-        console.log(negocicacao.data.getDate() + "/" +
-            (negocicacao.data.getMonth() + 1) + "/" +
-            negocicacao.data.getFullYear());
+        console.log(helper.dateToText(negocicacao.data));
     }
 }
---
+++ Formacao_front_end/JavaScript/CursoJavaScriptAvançado_I/client/js/app/helpers/DateHelper.js
@@ -0,0 +1,69 @@
+class DateHelper {
+
+    dateToText(date) {
+        console.log(date);
+        return date.getDate() + "/" +
+            (date.getMonth() + 1) + "/" +
+            date.getFullYear();
+    }
+
+    textToDate(text) {
+        console.log(typeof(text));
+        console.log(text);
+        console.log("//1º - Forma - usando aplit");
+        console.log(new Date(text.split("-")));
+        console.log("-------------------------------------------------------------------------------------------------");
+        console.log("//2º - Forma - usando replace com regex");
+        console.log(new Date(text.replace(/-/g, ',')));
+        console.log("-------------------------------------------------------------------------------------------------");
+        console.log("//3º - Forma - usando split recuperando as casas");
+        let ano = parseInt(text.split("-")[0]);
+        let mes = parseInt(text.split("-")[1]) - 1;
+        let dia = parseInt(text.split("-")[2]);
+        console.log(new Date(ano, mes, dia));
+        console.log("-------------------------------------------------------------------------------------------------");
+        console.log("//4º - Forma - usando spread operator");
+        console.log(
+            new Date(
+                text
+                .split("-")
+                .map(function(item, indice) {
+                    if (indice == 1)
+                        return item - 1;
+                    return item;
+                })
+            ));
+        console.log("-------------------------------------------------------------------------------------------------");
+        console.log("//5º - Forma - usando spread operator - e calculo de módulo");
+        console.log(
+            new Date(
+                text
+                .split("-")
+                .map(function(item, indice) {
+                    return item - (item % 2);
+                })
+            ));
+        console.log("-------------------------------------------------------------------------------------------------");
+        console.log("//6º - Forma - usando arrow functions - e calculo de módulo");
+        console.log(
+            new Date(
+                text
+                .split("-")
+                .map((item, indice) => {
+                    return item - (item % 2);
+                })
+            ));
+        console.log("-------------------------------------------------------------------------------------------------");
+
+        console.log("//7º - Forma - usando arrow functions simplificada por só ter uma instrução - e calculo de módulo");
+        console.log(
+            new Date(
+                text
+                .split("-")
+                .map((item, indice) => item - (item % 2))
+            ));
+
+        return new Date(text.split("-"));
+    }
+
+}
