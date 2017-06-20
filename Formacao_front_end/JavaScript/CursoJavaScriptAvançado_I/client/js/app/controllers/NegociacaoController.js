15cb3093159bfd3bb2b5ce7c86cba3f39d55b9c8
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-06-19 21:39:33 -0300

    05 - Criando nosso ListModel - 04 Lidar com data é trabalhoso? Chame um ajudante! - avaScript avançado I: ES6, orientação a objetos e padrões de projetos

--- Formacao_front_end/JavaScript/CursoJavaScriptAvançado_I/client/js/app/controllers/NegociacaoController.js
+++ Formacao_front_end/JavaScript/CursoJavaScriptAvançado_I/client/js/app/controllers/NegociacaoController.js
@@ -20,21 +20,29 @@ class NegociacaoController {
         this._inputDate = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event) {
         event.preventDefault();
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        console.log(this._listaNegociacoes.getNegociacoes);
+        this._limpaFormulario();
+    }
 
-        let negocicacao = new Negociacao(
+    _criaNegociacao() {
+        return new Negociacao(
             DateHelper.textToDate(this._inputDate.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
-        console.log("-------------------------------------------------------------------------------------------------");
+    }
 
-        //adicionar uma negociação em uma lista
+    _limpaFormulario() {
+        this._inputDate.value = '';
+        this._inputQuantidade.value = '1';
+        this._inputValor.value = '0.0';
 
-        console.log(negocicacao);
-        console.log(DateHelper.dateToText(negocicacao.data));
+        this._inputDate.focus();
     }
 }
---
+++ Formacao_front_end/JavaScript/CursoJavaScriptAvançado_I/client/js/app/models/ListaNegociacoes.js
@@ -0,0 +1,13 @@
+class ListaNegociacoes {
+    constructor() {
+        this._negociacoes = [];
+    }
+
+    adiciona(negociacao) {
+        this._negociacoes.push(negociacao);
+    }
+
+    get getNegociacoes() {
+        return this._negociacoes;
+    }
+}
