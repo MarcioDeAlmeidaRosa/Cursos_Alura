15953a179487abea758dd3d1345defce7f07c9a6
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-06-22 00:20:36 -0300

    08 - Um pouco mais sobre o reduce - 05 - Temos o modelo, mas e a view? - JavaScript avançado I: ES6, orientação a objetos e padrões de projetos

--- Formacao_front_end/JavaScript/CursoJavaScriptAvançado_I/exercicioExtra/Controller/ArrayController.js
+++ Formacao_front_end/JavaScript/CursoJavaScriptAvançado_I/exercicioExtra/Controller/ArrayController.js
@@ -20,12 +20,6 @@ function somatorio(array) {
     return resultado;
 }
 
-//Produtorio usando reduce:
-console.log(produtorio(numeros));
-function produtorio (array) {
-    let resultado = numeros.reduce(function(total, num) { return total * num; }, 1);
-    return resultado;
-}
 
 //O total se inicia com o valor 1, definido pelo segundo parâmetro da função reduce.
 //É feita a primeira iteração, pegando o primeiro valor do array (1) :
@@ -33,3 +27,36 @@ function produtorio (array) {
 //Na terceira iteração, com o segundo valor do array (3):
 //Na segunda iteração, com o segundo valor do array (4):
 //E no final ele devolve para nós o valor 24 , que é o valor esperado do produtório!
+//Produtorio usando reduce:
+console.log(produtorio(numeros));
+function produtorio (array) {
+    let resultado = numeros.reduce(function(total, num) { return total * num; }, 1);
+    return resultado;
+}
+
+
+//Um pouco mais sobre o reduce
+//Vejamos um exemplo com reduce que soma todos os números de um array:
+console.log(produtorio(numeros));
+function produtorio (array) {
+    let resultado = numeros.reduce(function(total, num) { return total * num; });
+    return resultado;
+}
+//O resultado é 10. Contudo, muitas vezes queremos começar a operação 
+//considerando um valor de inicialização. Por exemplo, queremos realizar 
+//a mesma operação, só que dessa vez, queremos começar com o valor 5.
+console.log(produtorio(numeros));
+function produtorio (array) {
+    let resultado = numeros.reduce(function(total, num) { return total * num; }, 5);
+    return resultado;
+}
+//Desta vez, o resultado final é 15!
+//Sendo assim, nada nos impede de fazer o primeiro reduce passando 0:
+console.log(produtorio(numeros));
+function produtorio (array) {
+    let resultado = numeros.reduce(function(total, num) { return total * num; }, 0);
+    return resultado;
+}
+//O resultado será 10 também. Mas será que vale a pena passar o segundo 
+//parâmetro da função reduce já que neste caso ele não faz diferença? 
+//Tudo vai depender do seu gosto.
