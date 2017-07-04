305739ec0503136ad733b2d8ed94de70ca5946f8
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-07-04 20:27:50 -0300

    03 - Realizando o parse da resposta - 04 - Importando negociações - JavaScript Avançado II: ES6, orientação a objetos e padrões de projetos

--- Formacao_front_end/JavaScript/CursoJavaScriptAvançado_II/client/js/app/controllers/NegociacaoController.js
+++ Formacao_front_end/JavaScript/CursoJavaScriptAvançado_II/client/js/app/controllers/NegociacaoController.js
@@ -108,8 +108,18 @@ class NegociacaoController {
             if (xhr.readyState == 4) {
                 if (xhr.status == 200) {
                     console.log('Obtendo as negociações do servidor');
+                    console.log(xhr.responseText);
+                    let negociacoes = JSON.parse(xhr.responseText);
+                    console.log(negociacoes);
+
+                    negociacoes
+                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+                        .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));;
+                    this._mensagem.texto = "Negociações importada com sucesso.";
                 } else {
-                    console.log('Não foi possível obter as negociações do servidor');
+                    console.log('');
+                    console.log(xhr.responseText);
+                    this._mensagem.texto = "Não foi possível obter as negociações do servidor.";
                 }
             }
         };
