16f268dcc6b9c33cd8b6aa6c8ed99b5e1d31a742
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-07-08 13:34:45 -0300

    Ajustando pasta

---
+++ Formacao_front_end/Frameworks_MVC/Angular2_Parte_1/client/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app',//maneira de acessar o componente nas views
+    templateUrl: './app/app.component.html'
+})
+class AppComponent {
+    constructor(){
+        
+    }
+}
