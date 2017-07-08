951b14d43482e52e1905e07558f25b023bba147c
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-07-08 12:12:13 -0300

    06 - Ah se meu código funcionasse no Edge! - 06 - Considerações finais e exercícios bônus - JavaScript Avançado II: ES6, orientação a objetos e padrões de projetos

--- Formacao_front_end/JavaScript/CursoJavaScriptAvançado_II/client/js/app/models/Mensagem.js
+++ Formacao_front_end/JavaScript/CursoJavaScriptAvançado_II/client/js/app/models/Mensagem.js
@@ -1,15 +1,19 @@
-class Mensagem{
+class Mensagem {
 
     //valor default no parâmetro
-    constructor(texto = ''){
-        this._texto = texto;
+
+    //O Edge não 13 não suporta parâmetros opcionais
+    //então teremos que alterar a implementação do contrutor
+    // constructor(texto = ''){
+    constructor(texto) {
+        this._texto = texto || ''; // se texto for undefined, vai passar ''
     }
 
-    get texto(){
+    get texto() {
         return this._texto;
     }
 
-    set texto(texto){
+    set texto(texto) {
         this._texto = texto;
     }
 }
