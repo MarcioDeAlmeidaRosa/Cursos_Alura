89248b94e574ab164f3a028115b991930378744f
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-07-13 00:11:47 -0300

    07 - O Componente Principal - 01 - Bem começado, metade feito - Angular 2: webapps ainda mais poderosas parte 1

--- Formacao_front_end/Frameworks_MVC/Angular_II_Parte_I/client/app/main.ts
+++
@@ -1,7 +0,0 @@
-// Arquivo necessário pelo loader de módulo para 
-// definir qual o módulo principal da aplicação
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.model';
-const platform = platformBrowserDynamic();
-//defini o módulo inicial que será carregado
-platform.bootstrapModule(AppModule);
